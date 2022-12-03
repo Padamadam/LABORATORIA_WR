2df43488a881bf5ffa6c5d6350d4acf20a184b69
Adam Rybojad <01168912@pw.edu.pl>
2022-12-03 14:55:39 +0100

    Added PID algorithm, sensor changed to light intensity, run function changed to run_forever

--- python_test.py
+++ python_test.py
@@ -1,48 +1,64 @@
 #!/usr/bin/env python3
 import ev3dev.ev3 as ev3
 
-lm = ev3.LargeMotor('outA')
-rm = ev3.LargeMotor('outD')
 
-ls = ev3.ColorSensor(ev3.INPUT_4)
-rs = ev3.ColorSensor(ev3.INPUT_1)
+class LineFollower():
+    
+    def __init__(self):
+        self.btn = ev3.Button()
+        self.shut_down = False
+        
+    
+    def run(self):
+        
+        ls = ev3.ColorSensor(ev3.INPUT_4)
+        rs = ev3.ColorSensor(ev3.INPUT_1)
+        
+        ls.mode = 'COLOR-REFLECT'
+        ls.mode = 'COLOR-REFLECT'
 
-ls.mode = "RGB-RAW"
-rs.mode = "RGB-RAW"
+        lm = ev3.LargeMotor('outA')
+        rm = ev3.LargeMotor('outD')
 
-lm.run_timed(time_sp=3000, speed_sp=500)
-rm.run_timed(time_sp=3000, speed_sp=500)
+        # lm.run_forever(time_sp=3000, speed_sp=500)
+        # rm.run_timed(time_sp=3000, speed_sp=500)
+        
+        speed = 100
+        
+        Kp = 0.5
+        dt = 0.01
+        Ki = 0.5
+        Kd = 0.5
 
-# while True:
-#    ev3.Leds.set_color(ev3.Leds.LEFT, (ev3.Leds.GREEN, ev3.Leds.RED)[ts.value()])
+        integral = 0
+        previous_error = 0
 
-Kp = 0.5
-dt = 500
-Ki = 0.5
-Kd = 0.5
-v = 100
+        while not self.shut_down:
+            lvalue = ls.reflected_light_intensity
+            rvalue = rs.reflected_light_intensity
+            
+            error = lvalue - rvalue
+            
+            
+            integral += (error*dt)
+            derivative = (error-previous_error) / dt
 
-integral = 0
-derivative = 0
-previous_error = 0
+            u = int((Kp*error) + (Ki*integral) + (Kd*derivative))
 
-while True:
-    lvalue = max(ls.raw)
-    rvalue = max(rs.raw)
-    error = lvalue - rvalue
-    integral += error*dt
-    derivative += error-previous_error / dt
+            print(u)
 
-    u = int((Kp*error) + (Ki*integral) + (Kd*derivative))
-    u = min(max(u, 0), v)
+            if speed + u >= 500:
+                lm.run_forever(speed_sp=500)
+                rm.run_forever(speed_sp=-500)
+            elif speed + u <= -500:
+                lm.run_forever(speed_sp=-500)
+                rm.run_forever(speed_sp=500)
 
-    print(u)
-
-    if u > 0:
-        lm.run_timed(time_sp=dt, speed_sp=v-u)
-        rm.run_timed(time_sp=dt, speed_sp=v+u)
-    else:
-        lm.run_timed(time_sp=dt, speed_sp=v+u)
-        rm.run_timed(time_sp=dt, speed_sp=v-u)
-
-        previous_error = error
+            lm.run_forever(speed_sp=speed + u)
+            rm.run_forever(speed_sp=speed - u)
+            previous_error = error
+            
+            
+if __name__ == "__main__":
+    robot = LineFollower()
+    robot.run()
